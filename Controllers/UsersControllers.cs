1191e8768a03145796e00922f908260bf361c0af
Salt4f <47352634+Salt4f@users.noreply.github.com>
2022-04-26 19:08:30 +0200

    Update UsersControllers.cs

--- Controllers/UsersControllers.cs
+++ Controllers/UsersControllers.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
@@ -48,22 +49,26 @@ namespace AnyLearnServer.Controllers
             var client = _config.GetSection("Linkedin").GetValue<string>("CliendId");
             var secret = _config.GetSection("Linkedin").GetValue<string>("Secret");
 
-            string url = "https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=" +
-                code +
-                "&redirect_uri=" +
-                "http://localhost:3000/linkedin" +
-                "&client_id=" +
-                client +
-                "&client_secret=" +
-                secret;
-            _logger.LogInformation(HttpUtility.UrlEncode(url));
-            var res = await _httpClient.GetAsync(HttpUtility.UrlEncode(url));
+
+            var b = new UriBuilder("https://www.linkedin.com/oauth/v2/accessToken");
+            b.Port = -1;
+            var q = HttpUtility.ParseQueryString(string.Empty);
+            q["grant_type"] = "authorization_code";
+            q["code"] = code;
+            q["redirect_uri"] = "http://localhost:3000/linkedin";
+            q["cliend_id"] = client;
+            q["client_secret"] = secret;
+
+            b.Query = q.ToString();
+
+            _logger.LogInformation(b.ToString());
+            var res = await _httpClient.GetAsync(b.ToString());
             res.EnsureSuccessStatusCode();
             var body = await res.Content.ReadAsStringAsync();
             var json = JObject.Parse(body);
             string? token = json.GetValue("access_token")!.Value<string>();
 
-            url = "https://api.linkedin.com/v2/me?projection=(localizedFirstName,localizedLastName,profilePicture(displayImage~:playableStreams))";
+            string url = "https://api.linkedin.com/v2/me?projection=(localizedFirstName,localizedLastName,profilePicture(displayImage~:playableStreams))";
             var request = new HttpRequestMessage(HttpMethod.Get, url);
             request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
             res = await _httpClient.SendAsync(request);
